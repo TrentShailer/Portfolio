01bc7202ec211f6b8a272568e0f4136c42850b2b
Trent Shailer <trent.shailer@gmail.com>
2022-04-18 23:02:15 +1200

    Noise webp support

--- frontend/src/Noise.tsx
+++ frontend/src/Noise.tsx
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+function support_format_webp() {
+	var elem = document.createElement("canvas");
+
+	if (!!(elem.getContext && elem.getContext("2d"))) {
+		// was able or not to get WebP representation
+		return elem.toDataURL("image/webp").indexOf("data:image/webp") == 0;
+	} else {
+		// very old browser like IE 8, canvas not supported
+		return false;
+	}
+}
 
 export default function Noise() {
+	const [noise, setNoise] = useState({});
+	useEffect(() => {
+		if (support_format_webp()) {
+			setNoise({ backgroundImage: "url('noise.webp')" });
+		} else {
+			setNoise({ backgroundImage: "url('noise.png')" });
+		}
+	}, []);
 	return (
 		<div
 			style={{
@@ -10,8 +30,8 @@ export default function Noise() {
 				height: "100%",
 				zIndex: 10,
 				pointerEvents: "none",
-				backgroundImage: "url('noise.png')",
 				opacity: 0.2,
+				...noise,
 			}}
 		/>
 	);
